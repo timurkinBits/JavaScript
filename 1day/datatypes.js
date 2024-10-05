a9f8ce8103dfe2f722dddfe78e253dd4837b8b85
timofey <timonkin08@gmail.com>
2024-10-05 15:25:25 +0600

    task1

---
+++ 1day/datatypes.js
@@ -0,0 +1,4 @@
+console.log(typeof string_var);
+console.log(typeof bool_var);
+console.log(typeof undefined_var);
+console.log(typeof null_var);
---
+++ 1day/introduction.js
@@ -0,0 +1,11 @@
+// comments can make code readable
+
+// welcome to 30DaysOfJavaScript
+
+/* 
+comments can make code readable,
+ easy to use and informative 
+*/
+
+console.log('I am ' + myAge + ' years old.')
+console.log('You are ' + yourAge + ' years old.')
