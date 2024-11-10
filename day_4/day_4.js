139864f5ec060495bbb2fbd8d75fb1c5ded7cf64
timofey <timonkin08@gmail.com>
2024-11-10 11:26:32 +0600

    day7-easy

---
+++ day_4/day_4.js
@@ -0,0 +1,41 @@
+// 1
+
+// let age = prompt('Enter your age: ')
+// if (age < 18) {
+//     alert(`You are left with ${18 - age} years to drive.`)
+// } else {
+//     alert(`You are old enough to drive`)
+// }
+
+// 2
+
+// let yourAge = prompt('Enter your age: ')
+// let myAge = 16
+// if (yourAge > myAge) {
+//     alert(`You are ${yourAge - myAge} years older than me.`)
+// } else if (myAge > yourAge) {
+//     alert(`You are ${myAge - yourAge} years younger than me.`)
+// } else {
+//     alert(`We are the same age`)
+// }
+
+// 3
+
+// let a = 4
+// let b = 3
+// if (a > b) {
+//     alert('a is greater than b')
+// } else {
+//     alert('a is less than b')
+// }
+
+// a > b ? alert('4 is greater than 3') : alert('a is less than b')
+
+// 4
+
+let num = prompt('Enter a number: ')
+if (num % 2 == 0) {
+    alert(num + ' is an even number')
+} else {
+    alert(num + ' is an odd number')
+}
