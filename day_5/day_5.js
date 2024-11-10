074f4c5f4f130ab94e5351dfd45e0ae0954f4d0a
timofey <timonkin08@gmail.com>
2024-11-10 10:50:22 +0600

    day5-easy

---
+++ day_5/day_5.js
@@ -0,0 +1,128 @@
+// 1
+
+// let array = []
+
+// 2
+
+// let array2 = [1,2,3,4,5,6,7,8]
+
+// 3
+
+// console.log(array2.length)
+
+// 4
+
+// console.log(array2[0])
+// console.log(array2[array2.length/2-1])
+// console.log(array2[array2.length-1])
+
+// 5
+
+// let mixedDataTypes = [1, 'str', [2, 3], true, null, undefined]
+// console.log(mixedDataTypes.length)
+
+// 6
+
+let itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+
+// 7
+
+// console.log(itCompanies)
+
+// 8
+
+// console.log(itCompanies.length)
+
+// 9
+
+// console.log(itCompanies[0])
+// console.log(itCompanies[3])
+// console.log(itCompanies[itCompanies.length-1])
+
+// 10
+
+// console.log(itCompanies[0])
+// console.log(itCompanies[1])
+// console.log(itCompanies[2])
+// console.log(itCompanies[3])
+// console.log(itCompanies[4])
+// console.log(itCompanies[5])
+// console.log(itCompanies[6])
+
+// 11
+
+// console.log(itCompanies[0].toUpperCase())
+// console.log(itCompanies[1].toUpperCase())
+// console.log(itCompanies[2].toUpperCase())
+// console.log(itCompanies[3].toUpperCase())
+// console.log(itCompanies[4].toUpperCase())
+// console.log(itCompanies[5].toUpperCase())
+// console.log(itCompanies[6].toUpperCase())
+
+// 12
+
+// console.log(itCompanies[0] 
+//     + ', ' + itCompanies[1] 
+//     + ', ' + itCompanies[2] 
+//     + ', ' + itCompanies[3] 
+//     + ', ' + itCompanies[4] 
+//     + ', ' + itCompanies[5] 
+//     + ' and ' + itCompanies[6] + ' are big IT companies.')
+
+// 13
+
+// if (itCompanies.includes('Google')) {
+//     console.log('Google')
+// } else {
+//     console.log('a company is not found')
+// }
+
+// 14
+
+// itCompanies[0]
+
+// 15
+
+// itCompanies.sort()
+
+// console.log(itCompanies)
+
+// 16
+
+// itCompanies.reverse()
+
+// console.log(itCompanies)
+
+// 17
+
+// console.log(itCompanies.slice(0, 3))
+
+// 18
+
+// console.log(itCompanies.slice(itCompanies.length - 3))
+
+// 19
+
+// console.log(itCompanies.slice(itCompanies.length/2, itCompanies.length/2+1))
+
+// 20
+
+// itCompanies.shift()
+
+// console.log(itCompanies)
+
+// 21
+
+// itCompanies.splice(itCompanies.length/2, 1)
+
+// console.log(itCompanies)
+
+// 22
+
+// itCompanies.pop()
+
+// console.log(itCompanies)
+
+// 23
+
+// console.log(itCompanies.splice())
